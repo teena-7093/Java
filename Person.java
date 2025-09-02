0db1203676ec3e6360acb6db9afd205f9de8a85d
Parimi Teena Sree <239x1a3371@gprec.ac.in>
2025-09-02 17:20:36 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,12 @@
+package day_2;
+
+public class Person {
+	String name;
+	int age;
+	static String nationality = "India";
+	public static void main(String[] args) {
+		System.out.println(nationality);
+		System.out.println(Person.nationality);
+		//System.out.println(name);
+	}
+}
